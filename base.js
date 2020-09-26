b44a4445be136b50e6ba105a96ee100f8ae4d607
DeadRisingStar <33833269+DeadRisingStar@users.noreply.github.com>
2020-09-26 08:30:00 -0400

    Update base.js

--- base.js
+++ base.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const BOT_TOKEN = process.env.TOKEN;
+const TOKEN = process.env.TOKEN;
 
-bot.login(BOT_TOKEN);
+bot.login(TOKEN);
 
 bot.on('ready', () => {
     console.info(`Logged in as ${bot.user.tag}!`);
