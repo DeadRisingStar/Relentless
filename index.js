2e9877a6f1d86850e5fa70b692be27788179e014
DeadRisingStar <33833269+DeadRisingStar@users.noreply.github.com>
2020-09-26 05:57:40 -0400

    Startup program

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ global.webhook = require('./src/miscellaneous/webhooklogger')
 global.sentry = require('./src/miscellaneous/sentry')
 
 require('./src/miscellaneous/start')
-require('dotenv').config()
+require('dotenv').config() 
+console.log('Starting node init')
+require('./src/bot/index')
+
 
