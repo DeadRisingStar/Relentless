a4bc946048c634259ac9f3106df423e9b0642032
DeadRisingStar <33833269+DeadRisingStar@users.noreply.github.com>
2020-09-26 10:11:09 -0400

    Error - Emergency FIx

--- base.js
+++ base.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-const { config } = require('bluebird');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const config = require("./config.json");
@@ -11,9 +10,9 @@ bot.login(TOKEN);
 bot.on('ready', () => {
     console.info(`Logged in as ${bot.user.tag}!`);
     if (config.activity.streaming == true) {
-    bot.user.setActivity("with depression", {url: 'https://twitch.tv/deadrisingstar'});
+    bot.user.setActivity("with depression", {url: 'https://www.twitch.tv/deadrisingstar'});
     } else {
-        bot.user.setActivity('depression', {type: "WATCHING" });
+        bot.user.setActivity('depression', {type: "WATCHING"});
     }
   });
 
