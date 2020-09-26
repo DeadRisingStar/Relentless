98baf75c096b187ee6c9a23748d4c97f21048eb7
DeadRisingStar <33833269+DeadRisingStar@users.noreply.github.com>
2020-09-26 09:01:30 -0400

    Update base.js

--- base.js
+++ base.js
@@ -8,11 +8,11 @@ bot.login(TOKEN);
 
 bot.on('ready', () => {
     console.info(`Logged in as ${bot.user.tag}!`);
-    bot.user.setStatus('available')
-    bot.user.setPresence({
+    client.user.setPresence({
+        status: "online",  //You can show online, idle....
         game: {
-            name: 'with depression',
-            type: "STREAMING",
+            name: "with depression",  //The message shown
+            type: "STREAMING", //PLAYING: WATCHING: LISTENING: STREAMING:
             url: "https://www.twitch.tv/DeadRisingStar"
         }
     });
