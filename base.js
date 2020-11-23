55dd08fbf1ddd1ca661c9c4761e15d9a200f7598
DeadRisingStar <33833269+DeadRisingStar@users.noreply.github.com>
2020-11-23 06:47:48 -0500

    Update base.js

--- base.js
+++ base.js
@@ -10,28 +10,9 @@ bot.on('ready', () => {
     console.info(`Logged in as ${bot.user.tag}!`);
     if (config.activity.streaming == true) {
     bot.user.setActivity("with depression :) V4", {url: 'https://www.twitch.tv/deadrisingstar'}, {type: "STREAMING"});
-    } else {
-        bot.user.setActivity('depression V3', {type: "WATCHING"});
     }
   });
   
-  client.on('message', (message) => {
-    if (message.content == '/ma') {
-        let channel = message.member.voiceChannel;
-        for (let member of channel.members) {
-            member[1].setMute(true)
-        }
-     }
-  });
-
-  client.on('message', (message) => {
-    if (message.content == '/um') {
-        let channel = message.member.voiceChannel;
-        for (let member of channel.members) {
-            member[1].setMute(false)
-        }
-     }
-  });
 
 
 
